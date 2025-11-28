7ba98cff63d3c06ba6b93689a48b4d189b5776fb
hamzaelm0401 <hamzaelmoumen2@gmail.com>
2025-11-28 14:59:22 +0100

    Fix vercel config and vite build

--- vite.config.ts
+++ vite.config.ts
@@ -20,9 +20,10 @@ export default defineConfig({
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, 'client'),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
+  outDir: 'dist',
+  emptyOutDir: true,
   },
+
   server: {
     port: 3000,
     strictPort: false,
